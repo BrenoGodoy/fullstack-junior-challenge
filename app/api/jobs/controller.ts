15b7604d2dceaf59037a643853281cc4643dfb8e
Breno Godoy <godoyy25z@gmail.com>
2024-04-11 03:45:34 -0300

    feat: Cria o filtro que retorna os jobs filtrados por nível

--- app/api/jobs/controller.ts
+++ app/api/jobs/controller.ts
@@ -1,6 +1,13 @@
 import { jobs } from "@/app/lib/jobs";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const getAll = async (req: Request, res: Response) => {
+export const getAll = async (req: NextRequest, res: NextResponse) => {
+  const query = req.nextUrl.searchParams.get('level');
+  if (query) {
+    const filteredJobs = jobs.filter((job) => job.level === query);
+
+    return NextResponse.json(filteredJobs);
+  };
+
   return NextResponse.json(jobs);
 }
