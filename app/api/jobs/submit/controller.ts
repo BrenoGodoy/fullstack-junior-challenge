d2f579a6cc9636aafc6df317790617806597dd35
Breno Godoy <godoyy25z@gmail.com>
2024-04-11 18:00:35 -0300

    fix: Remove importações desnecessárias

--- app/api/jobs/submit/controller.ts
+++ app/api/jobs/submit/controller.ts
@@ -1,4 +1,3 @@
-import { jobs } from "@/app/lib/jobs";
 import { NextRequest, NextResponse } from "next/server";
 
 export const submit = async (req: NextRequest, res: NextResponse, next: any, body?: any) => {
