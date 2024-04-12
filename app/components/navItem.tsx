8dda25277f097eeb8bd15e1ea899558761728c4f
Breno Godoy <godoyy25z@gmail.com>
2024-04-12 02:10:41 -0300

    feat: Altera cor do texto dos items do Nav

--- app/components/navItem.tsx
+++ app/components/navItem.tsx
@@ -1,7 +1,7 @@
 export default function NavItem({ children }: any) {
   return (
     <div className="relative">
-      <span className="cursor-pointer hover:text-custom-primary-500">{children}</span>
+      <span className="cursor-pointer hover:text-custom-primary-500 text-[#403C39]">{children}</span>
     </div>
   );
 };
