7ebe638b741b81f362c3e9ba0a6b8deab4a6b40b
Breno Godoy <godoyy25z@gmail.com>
2024-04-11 23:52:09 -0300

    feat: Adiciona mais validações de zod e personaliza mensagem de erro para cada caso

--- app/api/middlewares/index.ts
+++ app/api/middlewares/index.ts
@@ -8,31 +8,31 @@ export const secretMiddleware = async (req: NextRequest, res: NextResponse, next
     const secret = req.headers.get('secret');
   
     if (secret !== process.env.SECRET) {
-      return NextResponse.json({message: "Usuário não autorizado!"}, {status: Status.UNAUTHORIZED});
+      return NextResponse.json({message: 'Usuário não autorizado!'}, {status: Status.UNAUTHORIZED});
     }
 
    return next(req);
   } catch (error: any) {
-    if (error.message === 'Unexpected end of JSON input') {
-      return NextResponse.json({message: "Usuário não autorizado!"}, {status: Status.UNAUTHORIZED});
-    }
-
     return NextResponse.json({ message: error.message }, { status: Status.INTERNAL_SERVER_ERROR });
   }
 }
 
 export const verifySubmitBody = async (req: NextRequest, res: NextResponse, next: Function) => {
   try {
-    const body = await req.json();    
+    const body = await req.json();
+    
     RequestBodySchema.parse(body);
 
     return next(body);
   } catch (error: any) {
     if (error instanceof z.ZodError) {
-      return NextResponse.json({ message: "Corpo da requisição inválido!" }, { status: Status.BAD_REQUEST });
-    }
-    if (error.message === 'Unexpected end of JSON input') {
-      return NextResponse.json({message: "Usuário não autorizado!"}, {status: Status.UNAUTHORIZED});
+      const validationErrors = error.errors.map((validationError: any) => {
+        const field = validationError.path.join('.');
+        const message = validationError.message;
+        return `${field}: ${message}`;
+      });
+      
+      return NextResponse.json({ message: `Erro de validação do corpo da requisição: ${validationErrors.join(', ')}` }, { status: Status.BAD_REQUEST });
     }
 
     return NextResponse.json({ message: error.message }, { status: Status.INTERNAL_SERVER_ERROR });
--- app/api/schemas/index.ts
+++ app/api/schemas/index.ts
@@ -1,9 +1,13 @@
 import { z } from "zod";
 
+const phoneRegex = /^\(\d{2}\) \d{4,5}\-\d{4}$/;
+
 export const RequestBodySchema = z.object({
-  name: z.string(),
-  age: z.number(),
-  phone: z.string(),
-  state: z.string(),
-  city: z.string()
-});
+  name: z.string().min(1),
+  age: z.number().min(1),
+  phone: z.string().min(1).refine((value) => phoneRegex.test(value), {
+    message: 'Invalid phone number format (expected format: (00) 0000-0000)'
+  }),
+  state: z.string().min(1),
+  city: z.string().min(1)
+}).strict();
