4f6fde046cef2b98b36cdc26b8bf0744748f1074
Breno Godoy <godoyy25z@gmail.com>
2024-04-11 22:29:50 -0300

    fix: Altera rota com /[id] e submit para /job e não mais /jobs. Cria também constantes de statushttp e adiciona dotenv ao projeto

--- app/api/middlewares/index.ts
+++ app/api/middlewares/index.ts
@@ -1,22 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { RequestBodySchema } from "../schemas";
+import { Status } from "../utils/status";
 
 export const secretMiddleware = async (req: NextRequest, res: NextResponse, next: Function) => {
   try {
-    const body = await req.json();
+    const secret = req.headers.get('secret');
 
-    if (body.secret !== 'naranja-labs') {
-      return NextResponse.json({message: "Usuário não autorizado!"}, {status: 401});
+    if (secret !== process.env.SECRET) {
+      return NextResponse.json({message: "Usuário não autorizado!"}, {status: Status.UNAUTHORIZED});
     }
 
-   return next(body);
+   return next();
   } catch (error: any) {
     if (error.message === 'Unexpected end of JSON input') {
-      return NextResponse.json({message: "Usuário não autorizado!"}, {status: 401});
+      return NextResponse.json({message: "Usuário não autorizado!"}, {status: Status.UNAUTHORIZED});
     }
 
-    return NextResponse.json({ message: error.message }, { status: 500 });
+    return NextResponse.json({ message: error.message }, { status: Status.INTERNAL_SERVER_ERROR });
   }
 }
 
@@ -27,12 +28,12 @@ export const verifySubmitBody = async (req: NextRequest, res: NextResponse, next
     return next(body);
   } catch (error: any) {
     if (error instanceof z.ZodError) {
-      return NextResponse.json({ message: "Corpo da requisição inválido!" }, { status: 400 });
+      return NextResponse.json({ message: "Corpo da requisição inválido!" }, { status: Status.BAD_REQUEST });
     }
     if (error.message === 'Unexpected end of JSON input') {
-      return NextResponse.json({message: "Usuário não autorizado!"}, {status: 401});
+      return NextResponse.json({message: "Usuário não autorizado!"}, {status: Status.UNAUTHORIZED});
     }
 
-    return NextResponse.json({ message: error.message }, { status: 500 });
+    return NextResponse.json({ message: error.message }, { status: Status.INTERNAL_SERVER_ERROR });
   }
 }
