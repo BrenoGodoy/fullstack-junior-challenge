6da0e1d9528313453d4561c027b60fcc701ee82f
Breno Godoy <godoyy25z@gmail.com>
2024-04-12 01:49:47 -0300

    feat: Cria diferentes tonalidades de cores no tailwind.config e aplica essas mudanças

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
     <header className="flex flex-wrap justify-between items-center px-20 py-9">
       <div>
         <h1 className="text-4xl font-bold font-poppins">
-          <span className="text-custom-primary">Finest</span>
-          <span className="text-custom-secondary">Mart</span>
+          <span className="text-custom-primary-500">Finest</span>
+          <span className="text-custom-secondary-500">Mart</span>
         </h1>
       </div>
       <nav className="flex space-x-6">
@@ -20,8 +20,8 @@ export default function Header() {
       </nav>
       <div className="flex items-center space-x-5">
         <Image src="/market.png" alt="market" width={32} height={32}></Image>
-        <a href="#" className="text-custom-primary focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4">Sign In</a>
-        <a href="#" className="text-white bg-custom-primary rounded-lg text-sm px-7 py-3">Sign Up</a>
+        <a href="#" className="text-custom-primary-500 hover:text-custom-primary-200 font-medium rounded-lg text-sm px-4">Sign In</a>
+        <a href="#" className="text-white bg-custom-primary-500 hover:bg-custom-primary-200 rounded-lg text-sm px-7 py-3">Sign Up</a>
       </div>
     </header>
   );
