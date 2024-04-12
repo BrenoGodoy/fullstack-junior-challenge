9e74701a07a8274d4c9f95f5e8eafe8045fe7bc0
Breno Godoy <godoyy25z@gmail.com>
2024-04-12 19:14:12 -0300

    feat: Inicia criação dos items da lateral

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/header/header";
 import HomeImage from "./components/image/homeImage";
-import SearchBar from "./components/searchBar/searchBar";
+import Item from "./components/item/item";
 import Title from "./components/title/title";
 
 export default function Home() {
@@ -10,6 +10,10 @@ export default function Home() {
       <div className="flex flex-wrap">
         <Title></Title>
         <HomeImage></HomeImage>
+        <div className="flex flex-col flex-grow justify-around">
+          <Item></Item>
+          <Item></Item>
+        </div>
       </div>
     </section>
   );
