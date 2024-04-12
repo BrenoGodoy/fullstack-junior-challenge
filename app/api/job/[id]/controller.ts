622256194e20993f4e2504341b64c15d6dc2df6b
Breno Godoy <godoyy25z@gmail.com>
2024-04-11 23:06:39 -0300

    feat: Adiciona statusHttp como  constante para o retorno das rotas

--- app/api/job/[id]/controller.ts
+++ app/api/job/[id]/controller.ts
@@ -1,12 +1,13 @@
 import { jobs } from "@/app/lib/jobs";
 import { NextRequest, NextResponse } from "next/server";
+import { Status } from "../../utils/status";
 
 export const getById = async (req: NextRequest, res: NextResponse) => {
   const id = req.url.split('jobs/')[1];
   const job = jobs.find(job => job.id === parseInt(id));
 
   if (!job) {
-    return NextResponse.json({ message: "Job não encontrado!" }, { status: 404 });
+    return NextResponse.json({ message: "Job não encontrado!" }, { status: Status.BAD_REQUEST });
   }
 
   return NextResponse.json(job);
