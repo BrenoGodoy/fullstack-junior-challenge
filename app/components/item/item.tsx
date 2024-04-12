22785b5a2b1115eb75d0033386094c6036b20372
Breno Godoy <godoyy25z@gmail.com>
2024-04-12 19:23:15 -0300

    feat: Finaliza criação dos cards com imagem e background correto

--- app/components/item/item.tsx
+++ app/components/item/item.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 
-export default function Item() {
+export default function Item({title, price, imageUrl}: any) {
   return (
-    <section className="flex flex-col justify-around h-[254px] w-full bg-[#FBFBFB] rounded-[60px] flex justify-center items-center border-2 border-[#949494">
+    <section className="flex flex-col justify-around h-[254px] w-full bg-custom-primary-50 bg-opacity-50 rounded-[60px] flex justify-center items-center border-2 border-[#949494">
       <div>
-        <Image src="https://s3-alpha-sig.figma.com/img/b049/f29a/1dc6073ed8422b28ba5c657cc5c0081c?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fNho8FiGDO7Fc1aHuHhbWlob4bmTdUTKXGncXur-CHxcCiP-Lacs~0xU5eeipoE7tD5jg8UgUXoNwawyUFT6JD0Z7q92TcSLptP126f7I7rMGL8~4vRcbrDxaFUNnmCDIraXX1dwLy5xDDQ~jGsl20TonfI-xplWBFDiRx6FXguV7rPIi65AsZT~HqqiWiq4CzvVcSwRg-9gvTcrXYveFPXiUarNs80f5b1j3zmjls3Zzb1ms8DDajsPfx-MV4wC-eem3kd9ph5OHpdKe9-iyy36r783fp1drmGWt9Af9z8xY50ziHUilelCCZxI4IWTTXycC0qUCjhokJJJbqcT0Q__" alt="Imagem" className="max-w-full max-h-full rounded-lg" width={140} height={120}> 
+        <Image src={imageUrl} alt="item-image" width={140} height={120}> 
         </Image>
       </div>
       <div className="flex flex-col items-center">
-        <span>Fresh Spinach</span>
-        <span className="text-custom-secondary-500">$ 9.00</span>
+        <span>{title}</span>
+        <span className="text-custom-secondary-500">$ {price}</span>
       </div>
     </section>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
         <Title></Title>
         <HomeImage></HomeImage>
         <div className="flex flex-col flex-grow justify-around">
-          <Item></Item>
-          <Item></Item>
+          <Item title="Fresh Spinach" price="12.00" imageUrl="https://s3-alpha-sig.figma.com/img/b049/f29a/1dc6073ed8422b28ba5c657cc5c0081c?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fNho8FiGDO7Fc1aHuHhbWlob4bmTdUTKXGncXur-CHxcCiP-Lacs~0xU5eeipoE7tD5jg8UgUXoNwawyUFT6JD0Z7q92TcSLptP126f7I7rMGL8~4vRcbrDxaFUNnmCDIraXX1dwLy5xDDQ~jGsl20TonfI-xplWBFDiRx6FXguV7rPIi65AsZT~HqqiWiq4CzvVcSwRg-9gvTcrXYveFPXiUarNs80f5b1j3zmjls3Zzb1ms8DDajsPfx-MV4wC-eem3kd9ph5OHpdKe9-iyy36r783fp1drmGWt9Af9z8xY50ziHUilelCCZxI4IWTTXycC0qUCjhokJJJbqcT0Q__"></Item>
+          <Item title="Fresh Carrot" price="9.00" imageUrl="https://s3-alpha-sig.figma.com/img/7e82/a86f/b89f012b47052be62bfcad1d9f6ad87b?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=G63p2A0e7sYSM6RVsoBFD2Rq~rYI4NVxtv8pUdu6X7kX1duOuPE3bMri4TwlXvZ5czq2zoAcoe9lS~ktP4vNhST~8CTC8tifyzaL28EOS1BlYkm23Yw2CKXnmmQNWF3Cf21gBUnkBcTNPBGTak7VlX9vNkofKDPsuZttJmDb1G1aQVhpL9SmUG0wQD~oiffXdEp2yHY~tIE4Xrrk2rj2V1sKYGhU~Nks3p0irvO2iCdyjs6AplHoejiK~A3PuK3Lxk88RP8-lhetA7GdvLTdsIekFpJP1qvrAmCdtvMVXnnQ4qIkKk5mbgGgBcXOc2vSKBPKM58vMW~~yD7rjgHjOg__"></Item>
         </div>
       </div>
     </section>
