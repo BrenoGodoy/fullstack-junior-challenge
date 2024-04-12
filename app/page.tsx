b4fa8d004ca718f2ee79ce609d258631398cb6e5
Breno Godoy <godoyy25z@gmail.com>
2024-04-12 03:04:14 -0300

    feat: Começa a criação do Title

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/header";
+import Title from "./components/title/title";
 
 export default function Home() {
   return (
     <section className="min-h-screen lg:mx-12 xl:mx-28 custom-2xl:mx-48">
       <Header></Header>
+      <Title></Title>
     </section>
   );
 }
